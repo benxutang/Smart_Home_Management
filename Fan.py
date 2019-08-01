45bd95890269fc10f204d890848624c66b78adc8
TANGBEN7 <52163487+TANGBEN7@users.noreply.github.com>
2019-08-01 15:14:35 +0800

    Update Fan.py

--- Fan.py
+++ Fan.py
@@ -4,7 +4,8 @@ import signal
 from Adafruit_BME280 import *
 import time
 from gpiozero import MCP3008
-
+pot = MCP3008(1)
+state=2
 def Caculator(sensordata):
     if sensordata >= 28:
         fan = 100
@@ -17,6 +18,7 @@ def Caculator(sensordata):
 servopin = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(servopin, GPIO.OUT, initial = False)
 
 p = GPIO.PWM(servopin, 100)
@@ -28,10 +30,26 @@ while True:
     #pascals = sensor.read_pressure()
     #hectopascals = pascals / 100
     #humidity = sensor.read_humidity()
-    time.sleep(1)
+    time.sleep(0.1)
     print ('Temp      = {0:0.3f} deg C'.format(degrees))
-    #print ('Pressure  = {0:0.2f} hPa'.format(hectopascals))
+    #print ('Pressure  = {0:0.2f} hPa'.format(hectopascals))a
     #print ('Humidity  = {0:0.2f} %'.format(humidity))
 
-    servalue = Caculator(degrees)
-    p.ChangeDutyCycle(servalue)    
+    a=int(GPIO.input(23))
+    if a>0:
+        print("sb")
+        if state>=2:
+            state=0
+        else:
+            state=state+1
+    if state==0:
+        p.ChangeDutyCycle(0)
+    elif state==1:
+        servalue = Caculator(degrees)
+        p.ChangeDutyCycle(servalue)
+    else:
+        print(int(pot.value*20)*5)
+        p.ChangeDutyCycle(int(pot.value*20)*5)
+        time.sleep(0.1)       
+
+        
